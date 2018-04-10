80b763a0cbcfae2b75efd4c30d77a022eedec0e4
MrIngelborn <mringelborn@gmail.com>
2018-04-10 21:09:12 +0200

    Temporary page to register users

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/* User portal, requires login */
+require('is_logged_in.php');
+$is_admin = $_SESSION['admin'];
+
+
+?>
+
+
+<html>
+	<head>
+		<title>MoS LAN System</title>
+	</head>
+	<body>
+		<h1>User portal</h1>
+		
+		
+	</body>
+</html>
