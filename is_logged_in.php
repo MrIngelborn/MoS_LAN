98b882c439f52366dbf5c975a105e08e4e198bfa
MrIngelborn <mringelborn@gmail.com>
2018-04-25 01:25:25 +0200

    Change login check to look at user session variable. Added separate times for total login time and time since last activity

--- is_logged_in.php
+++ is_logged_in.php
@@ -1,14 +1,29 @@
 <?
 session_start();
 
+// Time from login until logout
+$max_time_since_login = 86400; // 24 h
+// Time from last ative until logout
+$max_time_since_last_active = 300; // 5 min
+
 function is_logged_in() {
-	if (!isset($_SESSION['login_time'])) return false;
-	if (time() - $_SESSION['login_time'] > 300) {
-		unset($_SESSION['login_time']);
+	// Check that a user is instanced
+	if (!isset($_SESSION['user'])) return false;
+	
+	// Check login time
+	if (time() - $_SESSION['login_time'] > $max_time_since_login) {
+		unset($_SESSION['user']);
 		return false;
 	}
+	
+	//Check time since last active
+	if (time() - $_SESSION['last_active_time'] > $max_time_since_last_active) {
+		unset($_SESSION['user']);
+		return false;
+	}
+	
 	// Update login time
-	$_SESSION['login_time'] = time();
+	$_SESSION['last_active_time'] = time();
 	return true;
 }
 
