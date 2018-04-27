da3de52a2586c81b16e5d3d6f1533b787da20c69
MrIngelborn <mringelborn@gmail.com>
2018-04-27 16:49:35 +0200

    Add default header information to Controllers

--- classes/Controller/Controllers/AbstractController.php
+++ classes/Controller/Controllers/AbstractController.php
@@ -3,16 +3,25 @@ namespace MoS\LAN\Controller\Controllers;
 
 abstract class AbstractController implements ControllerInterface
 {
-	protected $title = "MoS LAN";
-	protected $delimiter = "\t";
+	protected $title = 'MoS LAN';
+	protected $charset = 'UTF-8';
+	protected $css_links = array();
+	protected $js_links = array();
+	
+	public function __construct()
+	{
+		$this->css_links[] = '/css/spectre/spectre.min.css';
+		
+		$this->js_links[] = '/js/jquery/jquery-3.3.1.min.js';
+	}
 	
 	public function display(array $args = array())
 	{
 		echo '<!doctype html>', PHP_EOL;
 		echo '<html>', PHP_EOL;
-		echo '<header>', PHP_EOL;
-		echo $this->the_header();
-		echo '</header>', PHP_EOL;
+		echo '<head>', PHP_EOL;
+		$this->the_header();
+		echo '</head>', PHP_EOL;
 		echo '<body>', PHP_EOL;
 		echo $this->the_content(), PHP_EOL;
 		echo $this->the_footer(), PHP_EOL;
@@ -21,10 +30,23 @@ abstract class AbstractController implements ControllerInterface
 	}
 	
 	/**
-	* @return The content that should be within the header tag
+	* Prints the content that should be within the head tag
 	*/
 	protected function the_header() {
+		// Set charset
+		echo "<meta charset=\"$this->charset\">", PHP_EOL;
 		
+		// Print title
+		echo "<title>$this->title</title>", PHP_EOL;
+		 
+		// Print all css links
+		foreach($this->css_links as $link) {
+			echo "<link rel=\"stylesheet\" href=\"$link\">", PHP_EOL;
+		}
+		// Print all javascript links
+		foreach ($this->js_links as $link) {
+			echo "<script type=\"text/javascript\" src=\"$link\"></script>", PHP_EOL;
+		}
 	}
 	
 	/**
