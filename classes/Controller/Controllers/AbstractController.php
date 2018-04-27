9e7ceaac24b6854d974336df739632272db898d4
MrIngelborn <mringelborn@gmail.com>
2018-04-27 14:59:41 +0200

    fixed AbstractController to call its own functions

--- classes/Controller/Controllers/AbstractController.php
+++ classes/Controller/Controllers/AbstractController.php
@@ -8,14 +8,14 @@ abstract class AbstractController implements ControllerInterface
 	
 	public function display(array $args = array())
 	{
-		echo '<doctype html>', PHP_EOL;
+		echo '<!doctype html>', PHP_EOL;
 		echo '<html>', PHP_EOL;
 		echo '<header>', PHP_EOL;
-		echo the_header();
+		echo $this->the_header();
 		echo '</header>', PHP_EOL;
 		echo '<body>', PHP_EOL;
-		echo the_content(), PHP_EOL;
-		echo the_footer(), PHP_EOL;
+		echo $this->the_content(), PHP_EOL;
+		echo $this->the_footer(), PHP_EOL;
 		echo '</body>', PHP_EOL;
 		echo '</html>';
 	}
