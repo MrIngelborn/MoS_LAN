5c421b7461d8182829a0d9d3c9aee71a01751ace
MrIngelborn <mringelborn@gmail.com>
2018-04-27 14:51:14 +0200

    Added an abstract controller for controllers to extend

---
+++ classes/Controller/Controllers/AbstractController.php
@@ -0,0 +1,43 @@
+<?php
+namespace MoS\LAN\Controller\Controllers;
+
+abstract class AbstractController impements ControllerInterface
+{
+	protected $title = "MoS LAN";
+	protected $delimiter = "\t";
+	
+	public function display(array $args = array())
+	{
+		echo '<doctype html>', PHP_EOL;
+		echo '<html>', PHP_EOL;
+		echo '<header>', PHP_EOL;
+		echo the_header();
+		echo '</header>', PHP_EOL;
+		echo '<body>', PHP_EOL;
+		echo the_content(), PHP_EOL;
+		echo the_footer(), PHP_EOL;
+		echo '</body>', PHP_EOL;
+		echo '</html>';
+	}
+	
+	/**
+	* @return The content that should be within the header tag
+	*/
+	protected function the_header() {
+		
+	}
+	
+	/**
+	* @return The data that should be displayed on the bottom of the page
+	*/
+	protected function the_footer() {
+		
+	}
+	
+	/**
+	* @return The mail content of the page
+	*/
+	protected abstract function the_content();
+}
+
+?>
