bf3a8e752adea06508486c77a590e231e97b3e35
MrIngelborn <mringelborn@gmail.com>
2018-04-27 16:54:21 +0200

    Add a custom title to index page

--- classes/Controller/Controllers/IndexController.php
+++ classes/Controller/Controllers/IndexController.php
@@ -3,10 +3,19 @@ namespace MoS\LAN\Controller\Controllers;
 
 class IndexController extends AbstractController
 {
+	public function __construct()
+	{
+		// Set the deafult state
+		parent::__construct();
+		
+		$this->title = $this->title . ' | Home';
+	}
+	
 	/**
 	* @return The mail content of the page
 	*/
-	protected function the_content() {
+	protected function the_content()
+	{
 		return 'INDEX';
 	}
 
