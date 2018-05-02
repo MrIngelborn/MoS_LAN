41fa58bc49b9ed19ad1d9ec0b36fda377ca50e3a
MrIngelborn <mringelborn@gmail.com>
2018-05-02 21:30:03 +0200

    Changed to use the new ControllerInterface

--- classes/Controller/Controllers/PageController.php
+++ classes/Controller/Controllers/PageController.php
@@ -1,22 +1,21 @@
 <?php
 namespace MoS\LAN\Controller\Controllers;
 
-class PageController extends AbstractController
+use MoS\LAN\Routing\RequestInterface,
+	MoS\LAN\Routing\ResponseInterface;
+
+class PageController implements ControllerInterface
 {
 	public function __construct()
 	{
-		// Set the deafult state
-		parent::__construct();
 		
-		$this->title = $this->title . ' | Home';
 	}
 	
 	/**
-	* @return The mail content of the page
+	* Execute the given request
 	*/
-	protected function the_content()
-	{
-		return 'INDEX';
+	public function execute(RequestInterface $request, ResponseInterface $response){
+		echo 'INDEX';
 	}
 
 }
