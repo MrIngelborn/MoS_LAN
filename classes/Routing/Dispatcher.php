5f929f2da10d85c6a05ba57fc84e680b297b75cd
MrIngelborn <mringelborn@gmail.com>
2018-05-02 21:31:37 +0200

    Cleanup

--- classes/Routing/Dispatcher.php
+++ classes/Routing/Dispatcher.php
@@ -1,9 +1,9 @@
 <?php
 namespace MoS\LAN\Routing;
 
-class Dispatcher implements DispatcherInterface {
-
-	public function dispatch(RouteInterface $route, RequestInterface $request, ResponseInterface $response)
+class Dispatcher implements DispatcherInterface
+{
+	public function dispatch(?RouteInterface $route, RequestInterface $request, ResponseInterface $response)
 	{
 		$controller = $route->createController();
 		$controller->execute($request, $response);
