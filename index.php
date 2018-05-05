370a47b279b63c23a8c1e705d882ed2790254099
MrIngelborn <mringelborn@gmail.com>
2018-05-05 17:09:34 +0200

    Added router and database

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-use MoS\LAN\Helpers\PDOFactory,
-	MoS\LAN\FrontController;
+use MoS\LAN\FrontController;
 
 // Enable error reporting for testing
 ini_set('error_reporting', E_ALL);
@@ -8,9 +7,7 @@ ini_set('display_errors', 'true');
 
 require_once __DIR__.'/bootstrap.php';
 
-$db = PDOFactory::createFromYamlConfig(__DIR__.'/config/db.yaml');
-
-$controller = new FrontController($twig);
+$controller = new FrontController($twig, $router, $db);
 $controller->handleRequest();
 
 ?>
