ac701353b5813a45aad5777ae7071b4e8f43edb1
MrIngelborn <mringelborn@gmail.com>
2018-05-05 17:09:02 +0200

    Added router and database

--- app/src/FrontController.php
+++ app/src/FrontController.php
@@ -1,15 +1,20 @@
 <?php
 namespace MoS\LAN;
 
-use Twig\Environment;
+use Twig\Environment,
+    Bramus\Router\Router;
 
 class FrontController
 {
 	private $twigEnvironment;
+	private $router;
+	private $pdo;
 	
-	public function __construct(Environment $twig)
+	public function __construct(Environment $twig, Router $router, \PDO $pdo)
 	{
 		$this->twigEnvironment = $twig;
+		$this->router = $router;
+		$this->pdo = $pdo;
 	}
 	public function handleRequest()
 	{
