210b0072714f92a5f46ef4c793fefede051735e8
MrIngelborn <mringelborn@gmail.com>
2018-05-05 17:09:15 +0200

    Added router and database

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,6 @@
 <?php
+use MoS\LAN\Helpers\PDOFactory,
+    Bramus\Router\Router;
 
 // Setup my autoloader
 require_once __DIR__."/app/Psr4AutoloaderClass.php";
@@ -15,5 +17,11 @@ $loader = new Twig_Loader_Filesystem(__DIR__.'/app/src/Templates');
 
  // Instantiate Twig
 $twig = new Twig_Environment($loader);
+
+// Create database
+$db = PDOFactory::createFromYamlConfig(__DIR__.'/app/config/db.yaml');
+
+// Create Router instance
+$router = new Router();
 
 ?>
