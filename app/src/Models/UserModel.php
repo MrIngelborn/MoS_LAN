5e97facf8d140bc8ec0c87decf05bbe6f6e5ebfb
MrIngelborn <mringelborn@gmail.com>
2018-05-11 17:23:03 +0200

    Cleanup

--- app/src/Models/UserModel.php
+++ app/src/Models/UserModel.php
@@ -3,41 +3,44 @@ namespace MoS\LAN\Models;
 
 class UserModel extends AbstractModel implements Listable
 {
-	private const USER_TABLE = 'users';
-	private const INFO_TABLE = 'user_info';
-	
-	public function fetchById($id)
-	{
-		$userTable = self::USER_TABLE;
-		$infoTable = self::INFO_TABLE;
-		$query = "SELECT * FROM $userTable "
-				."JOIN $infoTable ON $userTable.id = $infoTable.user_id "
-				."WHERE id = :id LIMIT 1";
-		$params = array(':id' => $id);
-		$this->fetchData($query, $params);
-	}
-	public function fetchList()
-	{
-		$query = 'SELECT id, username FROM '.self::USER_TABLE;
-		$this->fetchData($query, null);
-	}
-	public function getList()
-	{
-		$list = array();
-		foreach ($this->data as $user) {
-			$list[] = array(
-				'href' => 'users/'.$user['id'],
-				'value' => $user['username']
-			);
-		}
-		return $list;
-	}
-	public function getListTitle()
-	{
-		return 'Users';
-	}
-	public function getListHeader()
-	{
-		return 'List of users';
-	}
+    private const USER_TABLE = 'users';
+    private const INFO_TABLE = 'user_info';
+    
+    public function fetchById($id)
+    {
+        $userTable = self::USER_TABLE;
+        $infoTable = self::INFO_TABLE;
+        $query = "SELECT * "
+                ."FROM $userTable "
+                ."JOIN $infoTable "
+                ."ON $userTable.id = $infoTable.user_id "
+                ."WHERE id = :id "
+                ."LIMIT 1";
+        $params = array(':id' => $id);
+        $this->fetchData($query, $params);
+    }
+    public function fetchList()
+    {
+        $query = 'SELECT id, username FROM '.self::USER_TABLE;
+        $this->fetchData($query, null);
+    }
+    public function getList()
+    {
+        $list = array();
+        foreach ($this->data as $user) {
+            $list[] = array(
+                'href' => 'users/'.$user['id'],
+                'value' => $user['username']
+            );
+        }
+        return $list;
+    }
+    public function getListTitle()
+    {
+        return 'Users';
+    }
+    public function getListHeader()
+    {
+        return 'List of users';
+    }
 }
